2a04b37f38daa9d787be08d1ab0aa85ab3f5a4d6
Jose Pacheco <jose@icheko.com>
2020-07-07 11:36:29 -0700

    fix issue when input is piped in from another command

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 		panic(err)
 	}
 
-	if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
+	if info.Mode()&os.ModeCharDevice != 0 {
 		fmt.Println("The command is intended to work with pipes.")
 		fmt.Println("Usage: cat file | " + packageName)
 		return
